d919c54b3729700c4bcaa558a8aaf07b7ed03569
Friedrich Tschirpke <friedrich.tschirpke@live.de>
2024-07-02 22:48:37 +0200

    chore: remove lazy comments and add type hints (graph_to_shapefile)

--- bin/e_graph_to_shapefile.py
+++ bin/e_graph_to_shapefile.py
@@ -6,6 +6,7 @@ import csv
 from datetime import datetime
 from pathlib import Path
 import sys
+from typing import Dict
 
 import fiona
 from geopandas import GeoDataFrame
@@ -16,7 +17,7 @@ from shapely.geometry import LineString, Point
 from b_extract_trough_transects import read_graph
 
 
-def create_point_shp_nodes(coord_dict, save_loc_shp):
+def create_point_shp_nodes(coord_dict: Dict, save_loc_shp: str):
     # define schema
     schema_pts = {
         'geometry': 'Point',
@@ -37,41 +38,39 @@ def create_point_shp_nodes(coord_dict, save_loc_shp):
     node_shp.close()
 
 
-def get_edge_info_csv(weighted_graph, csv_loc):
-    # Du versuchst hier ein CSV zu lesen was kein CSV ist zuvor. In anderen Skripten rufst du folgendes auf:
-    # nx.read_edgelist(edgelist_loc, data=True, create_using=nx.DiGraph())
-    # Damit solltest du die Edelist auslesen können
-    reader = csv.reader(open(weighted_graph), delimiter=";")
-    # open the file in the write mode
-    f = open(csv_loc, 'w', newline='')
+def get_edge_info_csv(weighted_graph: Path, csv_loc: str):
+    with open(weighted_graph) as graph_file, open(csv_loc, "w", newline="") as f:
+        reader = csv.reader(graph_file, delimiter=";")
+        writer = csv.writer(f)
 
-    # create the csv writer
-    writer = csv.writer(f)
-    header = ['start_node', 'end_node', 'pts', 'length', 'mean_width', 'median_width', 'mean_depth', 'median_depth', 'mean_r2', 'median_r2', 'considered_trans', 'water_filled']
-    writer.writerow(header)
-    for row in reader:
-        thisrow = []
-        thisrow.append(row[0])
-        thisrow.append(row[1])
-        row_as_string = row[2]
-        row_as_string = row_as_string.replace('nan', '"NULL"')
-        dicti = ast.literal_eval(row_as_string)
-        vals = list(dicti.values())
-        for i in list(range(len(vals))):
-            thisrow.append(vals[i])
-        writer.writerow(thisrow)
-    f.close()
+        header = [
+            'start_node', 'end_node', 'pts', 'length', 'mean_width', 'median_width',
+            'mean_depth', 'median_depth', 'mean_r2', 'median_r2',
+            'considered_trans', 'water_filled'
+        ]
+        writer.writerow(header)
+        for row in reader:
+            thisrow = []
+            thisrow.append(row[0])
+            thisrow.append(row[1])
+            row_as_string = row[2]
+            row_as_string = row_as_string.replace('nan', '"NULL"')
+            dicti = ast.literal_eval(row_as_string)
+            vals = list(dicti.values())
+            for i in list(range(len(vals))):
+                thisrow.append(vals[i])
+            writer.writerow(thisrow)
 
 
-def add_coords_to_edge_id(csv, coord_dict):
-    # print(coord_dict)
+def add_coords_to_edge_id(csv: str, coord_dict: Dict) -> pd.DataFrame:
     edge_info = pd.read_csv(csv)
-    # print(edge_info)
+
     start_lat = []
     start_lon = []
     end_lat = []
     end_lon = []
     geometry = []
+
     # iterate through starting nodes and grab coordinates from dictionary
     for edge in edge_info['start_node']:
         start_lat.append(int(coord_dict[str(edge)][0]))
@@ -90,13 +89,12 @@ def add_coords_to_edge_id(csv, coord_dict):
     edge_info['end_lat'] = end_lat
     edge_info['end_lon'] = end_lon
     edge_info['geometry'] = geometry
-    # print(edge_info)
 
     # output the dataframe so a shapefile can be built from it after
     return edge_info
 
 
-def create_line_shp_edges(edge_info_df, save_loc_shp):
+def create_line_shp_edges(edge_info_df: pd.DataFrame, save_loc_shp: str):
     edge_info_gdf = GeoDataFrame(edge_info_df, geometry=edge_info_df['geometry'])
     edge_info_gdf.to_file(save_loc_shp, mode='w', driver='ESRI Shapefile')
 
@@ -123,14 +121,14 @@ def main():
     aoiName = args.edgelistFile.stem
 
     # generate point-shapefile from nodes.
-    create_point_shp_nodes(coord_dict, aoiName + '_nodes.shp')
+    create_point_shp_nodes(coord_dict, f"{aoiName}_nodes.shp")
 
     # prepare for shapefiling edges
-    get_edge_info_csv(args.weightedGraphFile, aoiName + "_edges_csv.csv")
+    get_edge_info_csv(args.weightedGraphFile, f"{aoiName}_edges_csv.csv")
 
     # shapefile edges
-    edge_info_df = add_coords_to_edge_id(aoiName + "_edges_csv.csv", coord_dict)
-    create_line_shp_edges(edge_info_df, aoiName + '_edges.shp')
+    edge_info_df = add_coords_to_edge_id(f"{aoiName}_edges_csv.csv", coord_dict)
+    create_line_shp_edges(edge_info_df, f"{aoiName}_edges.shp")
 
 
 if __name__ == '__main__':
